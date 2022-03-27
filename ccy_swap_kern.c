07470a747f919c62b6fb64ab99535401e1dd3e93
Zoey <cuiziki@163.com>
2022-03-27 14:31:26 +0800

    step 1: test kretprobe and ktime

--- ccy_swap_kern.c
+++ ccy_swap_kern.c
@@ -7,18 +7,44 @@
 
 #define SEC(NAME) __attribute__((section(NAME), used))
 
+struct cost_ctx {
+	u64 do_swap_page; // start time of 'do_swap_page' being called.
+	u64 frontswap_load;// end time after 'frontswap_load' been called.
+	u64 swap_readpage;// end time after 'swap_readpage' been called.
+};
+
 struct bpf_map_def SEC("maps") ctx_map = {
 		.type = BPF_MAP_TYPE_HASH,
 		.key_size = sizeof(u32),
-		.value_size = sizeof(u32),
+		.value_size = sizeof(struct cost_ctx),
 		.max_entries = 1024,
 };
 
 SEC("kprobe/do_swap_page")
-int pre_do_page_fault(struct pt_regs *regs){
+int pre_swap_readpage(struct pt_regs *regs){
 		u32 kpid = bpf_get_current_pid_tgid() >>32;
-		char fmt[] = "pid: %u call do_swap_page!\n";
-		bpf_trace_printk(fmt, sizeof(fmt) , kpid);
+		u32 tpid = bpf_get_current_pid_tgid() & 0xFFFFFFFF;
+		u64 time = bpf_ktime_get_boot_ns();
+		char fmt[] = "pid: %u tpid:%u call do_swap_page!\n";
+		bpf_trace_printk(fmt, sizeof(fmt) , kpid, tpid);
+		return 0;
+}
+
+SEC("kretprobe/frontswap_load")
+int post_frontswap_load(struct pt_regs *regs){
+		u32 kpid = bpf_get_current_pid_tgid() >>32;
+		u32 tpid = bpf_get_current_pid_tgid() & 0xFFFFFFFF;
+		char fmt[] = "pid: %u tpid:%u after call frontswap_load!\n";
+		bpf_trace_printk(fmt, sizeof(fmt) , kpid, tpid);
+		return 0;
+}
+
+SEC("kretprobe/swap_readpage")
+int post_swap_readpage(struct pt_regs *regs){
+		u32 kpid = bpf_get_current_pid_tgid() >>32;
+		u32 tpid = bpf_get_current_pid_tgid() & 0xFFFFFFFF;
+		char fmt[] = "pid: %u tpid:%u after call swap_readpage!\n";
+		bpf_trace_printk(fmt, sizeof(fmt) , kpid, tpid);
 		return 0;
 }
 char _license[] SEC("license") = "GPL";
--- ccy_swap_user.c
+++ ccy_swap_user.c
@@ -5,7 +5,11 @@
 #include <bpf/bpf.h>
 #include "bpf_load.h"
 #include "bpf_util.h"
-
+struct cost_ctx {
+	long do_swap_page; // start time of 'do_swap_page' being called.
+	long frontswap_load;// end time after 'frontswap_load' been called.
+	long swap_readpage;// end time after 'swap_readpage' been called.
+};
 
 int main(int argc, char **argv)
 {
@@ -16,9 +20,13 @@ int main(int argc, char **argv)
 				return -1;
 		}
 		int fd = map_fd[0];
-		int value = 0;
 		pid_t pid = getpid();
 		printf("user space pid :%u\n",pid);
+		struct cost_ctx value = {
+			.do_swap_page=0,
+			.frontswap_load=0,
+			.swap_readpage=0,
+		};
 		int ret = bpf_map_update_elem(fd, &pid, &value, 0);
 		if(ret == 0)    {
 				printf("Insert value into bpf map success!\n");
